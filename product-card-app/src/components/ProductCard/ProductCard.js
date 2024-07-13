475210eff73370b82541326194cabcef629d1d17
shimaaFathi <shimaafathi126@gmail.com>
2024-07-14 02:16:58 +0300

    style-buttons

--- product-card-app/src/components/ProductCard/ProductCard.js
+++ product-card-app/src/components/ProductCard/ProductCard.js
@@ -4,21 +4,22 @@ const ProductCard = ({ title, description, stock, image }) => {
     return (
         <div className="product-card">
             <img src={image} alt={title} />
-            <h2>{title}</h2>
+            <h2>
+                {title}
+                <button className="favorite-button">
+                    <i className="fa fa-heart" aria-hidden="true"></i>
+                </button>
+            </h2>
             <p>{description}</p>
             <p className={`stock-status ${stock ? 'in-stock' : 'out-of-stock'}`}>
                 {stock ? 'In Stock' : 'Out of Stock'}
             </p>
-            <div className="button-container">
-                <button className="buy-button">
-                    <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-                </button>
-                <button className="favorite-button">
-                    <i className="fa fa-heart" aria-hidden="true"></i>
-                </button>
-            </div>
+            <button className="buy-button">
+                <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            </button>
         </div>
     );
 };
+
 
 export default ProductCard;
