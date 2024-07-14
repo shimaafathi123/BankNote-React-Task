1b7845219323905c680c7670b4e136e0fcdc1e23
shimaaFathi <shimaafathi126@gmail.com>
2024-07-14 11:22:36 +0300

    restyling

--- product-card-app/src/App.js
+++ product-card-app/src/App.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-            <h1>Product List</h1>
             <ProductList />
         </div>
   );
